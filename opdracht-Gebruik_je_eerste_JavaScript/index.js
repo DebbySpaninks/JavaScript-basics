8f99e56665e8cc0a06247ccd4943d5aab128e02a
DebbySpaninks <62060764+DebbySpaninks@users.noreply.github.com>
2020-08-11 23:24:15 +0200

    Add files via upload

---
+++ opdracht-Gebruik_je_eerste_JavaScript/index.js
@@ -0,0 +1,29 @@
+console.log("Hello Winc Academy")
+
+let name = "Debby Spaninks";
+console.log(name);
+
+let sum = 4 + 4;
+console.log(sum);
+
+let addition = 4 + 6;
+console.log(addition);
+
+let subtraction = 10 - 5;
+console.log(subtraction);
+
+let multiplication = 10 * 10;
+console.log(multiplication);
+
+let division = 10 / 5;
+console.log(division);
+
+let modulus = 4 % 10;
+console.log(modulus);
+
+let age = 37;
+console.log(typeof age);
+
+let ageText = "zevenendertig";
+console.log(typeof ageText);
+
