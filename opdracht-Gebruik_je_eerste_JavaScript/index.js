65a472fd59adf778b8924946744c6bdad952aa6a
DebbySpaninks <62060764+DebbySpaninks@users.noreply.github.com>
2020-08-11 22:28:45 +0200

    Add files via upload

---
+++ opdracht-Gebruik_je_eerste_JavaScript/index.js
@@ -0,0 +1,25 @@
+let name = "Debby Spaninks";
+console.log(name);
+
+let numberAddition = 4 + 4;
+console.log(numberAddition);
+
+let numberSubtraction = 10 - 5;
+console.log(numberSubtraction);
+
+let numberMultiplication = 10 * 10;
+console.log(numberMultiplication);
+
+let numberDivision = 10 / 5;
+console.log(numberDivision);
+
+let numberModulus = 4 % 10;
+console.log(numberModulus);
+
+let age = 37;
+console.log(age);
+
+//Maak een variabele met je leeftijd in cijfers(bijvoorbeeld 48), log * het type variabele naar de console met typeof (typeof operator)
+//Verander nu het type van deze variable door je leeftijd("48") uit te schrijven in "text", log het type naar de console met typeof .
+//Gebruik je externe script.js file in je HTML pagina.Maak een nieuwe script.js file en verplaats al je JavaScript tot nu toe naar deze file.
+//* wanneer we vragen om een uitkomst te "loggen" —> gebruik console.log(jouw uitkomst)
