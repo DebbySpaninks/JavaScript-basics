dcb6b6aa1a3e38a9a565775208a800bdd033c350
DebbySpaninks <62060764+DebbySpaninks@users.noreply.github.com>
2020-08-24 16:34:07 +0200

    Add files via upload

---
+++ W1D2-3/script.js
@@ -0,0 +1,11 @@
+console.log("Hallo Winc Academy studenten");
+
+//dit is een grote som
+
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+
+/*dit is block comment van 2 rekensommen
+console.log(1230941 * 1823794);
+console.log(637263 / 54); 
+*/
