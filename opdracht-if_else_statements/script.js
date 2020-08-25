2576a25f89810e7dc70be4d905c5d608077c46f2
DebbySpaninks <62060764+DebbySpaninks@users.noreply.github.com>
2020-08-25 16:46:02 +0200

    Add files via upload

---
+++ opdracht-if_else_statements/script.js
@@ -0,0 +1,27 @@
+const age = 16;
+
+if (age > 17) {
+  console.log("Je bent 18 jaar of ouder, kom maar binnen");
+} else {
+  console.log(
+    "Je bent jonger dan 18 jaar, je wordt vriendelijk verzocht buiten te blijven"
+  );
+}
+
+const isFemale = false;
+
+if (isFemale == true) {
+  console.log("Je bent een vrouw");
+} else {
+  console.log(
+    "Ladiesnight is alleen voor vrouwen, je kunt wel tickets kopen voor een andere dag"
+  );
+}
+
+const driverStatus = "bob";
+
+if (driverStatus == "bob") {
+  console.log("Jij bent de bob en mag rijden");
+} else {
+  console.log("Jij moet lopend naar huis of de bob brengt je weg");
+}
