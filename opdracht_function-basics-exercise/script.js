136acb142d8fe75b2ba3a654b4d6b80b543af7ed
DebbySpaninks <62060764+DebbySpaninks@users.noreply.github.com>
2021-02-04 15:57:31 +0100

    Add script.js

---
+++ opdracht_function-basics-exercise/script.js
@@ -0,0 +1,21 @@
+console.log('script.js is running with node');
+
+const makePizza = () => {
+    console.log('preheat oven');
+    console.log('make bottom');
+    console.log('add tomato sauce');
+    console.log('add vegetables');
+    console.log('bake pizza in oven');
+};
+
+const makeSushi = () => {
+    console.log('boil rice');
+    console.log('slice vegetables');
+    console.log('prepare fish');
+    console.log('make sushi rolls');
+};
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
