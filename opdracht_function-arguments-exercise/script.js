417b25532e8ce2058531bd3b172d2a05649f2364
DebbySpaninks <62060764+DebbySpaninks@users.noreply.github.com>
2021-02-05 11:12:17 +0100

    Update script.js

--- opdracht_function-arguments-exercise/script.js
+++ opdracht_function-arguments-exercise/script.js
@@ -3,6 +3,6 @@ console.log('this script runs');
 const paintWalls = (side, color) => {
     console.log('the ' + side + ' wall has been painted ' + color);
 };
-paintWalls('south', 'green');              // the wall has been painted green
-paintWalls('west', 'purple');              // the wall has been painted purple
-paintWalls();                              // undefined
+paintWalls('south', 'green');              // the south wall has been painted green
+paintWalls('west', 'purple');              // the west wall has been painted purple
+paintWalls();                              // the undefined wall has been painted undefined
